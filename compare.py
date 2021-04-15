d8876f7801a0947d9a95a51fc7cecd0526425467
KatarinaMarcincakova <marcincakova.katarina@gmail.com>
2021-04-15 13:12:46 +0200

    calc match [%]

--- compare.py
+++ compare.py
@@ -7,5 +7,11 @@ with open('files/file1.txt') as file_1, open('files/file2.txt') as file_2:
     differ_result_array = list(differ.compare(f1, f2))
     
     sum_of_lines = len(f1) + len(f2)
-    print(sum_of_lines)
-    for r in differ_result_array: print(f'# {r}')
+    sum_of_same_lines = 0
+
+    for r in differ_result_array:
+        if r[0] == '-': sum_of_same_lines += 2
+    
+    print(f'[SUM Of ALL LINES]: {sum_of_lines}')
+    print(f'[SUM Of SAME LINES]: {sum_of_same_lines}')
+    print(f'[MATCH %]: {(sum_of_same_lines / sum_of_lines) * 100}%')
